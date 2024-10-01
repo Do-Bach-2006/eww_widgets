2ccfd99814f4931945978c5e0b80d06f64b4a819
Do-Bach-2006 <dobachk5@gmail.com>
2024-10-01 17:40:29 +0700

    update

--- scripts/get_free_games.py
+++ scripts/get_free_games.py
@@ -1,88 +1,93 @@
 from requests import get
-from json import dump , load
+from json import dump, load, dumps, loads
 from datetime import date
 from os import chdir
+import http.client
 
 
 def main():
-    PATH_TO_EPIC_DATA = "./data/epic_free_games.json"
+    PATH_TO_EPIC_DATA = "../data/epic_free_games.json"
 
     data = None
 
-
     try:
         with open(PATH_TO_EPIC_DATA, "r") as file_obj:
-            data = load(file_obj)
+            try:
+                data = load(file_obj)
+            except:
+                data = {}
 
-            current = date.today()
+        current = date.today()
 
-            current_date = current.strftime("%Y-%m-%d")
+        current_date = current.strftime("%Y-%m-%d")
 
         # the date is called expired when the min date is the expiredate ( current date is bigger than or equal the expiredate)
-        if "expire_date" not in data or get_min_date( [data["expire_date"] , current_date ] ) == data["expire_date"]:
+        if (
+            "expire_date" not in data
+            or get_min_date([data["expire_date"], current_date]) == data["expire_date"]
+        ):
             with open(PATH_TO_EPIC_DATA, "w") as file_obj:
                 # update the json
                 data = get_epic_free_games_json()
                 dump(data, file_obj)
 
-
     except FileNotFoundError:
-
-
         with open(PATH_TO_EPIC_DATA, "w") as file_obj:
-
             data = get_epic_free_games_json()
+
             dump(data, file_obj)
 
+    if data != {}:
+        print(" , ".join(data["names"]))
 
-    print( " , ".join(data["names"]) )
+
+# FIXME: go and find an epic free games api that do not change over time ( recommend github ) and replace it with this current api
 
 
 def get_epic_free_games_json() -> dict:
+    conn = http.client.HTTPSConnection("epic-free-games1.p.rapidapi.com")
 
-    request = get('https://epic-free-games1.p.rapidapi.com/currentGames' , headers = {
-    'x-rapidapi-key': "3c80cd1d39msh4646d536d544796p186b57jsn362c1681c902",
-    'x-rapidapi-host': "epic-free-games1.p.rapidapi.com"
-        } )
+    headers = {
+        "x-rapidapi-key": "3c80cd1d39msh4646d536d544796p186b57jsn362c1681c902",
+        "x-rapidapi-host": "epic-free-games1.p.rapidapi.com",
+    }
 
+    conn.request("GET", "/currentGames", headers=headers)
 
-    if request.status_code == 200:
+    res = conn.getresponse()
+    data = res.read()
 
+    json_str = data.decode("utf-8")
+    # the res.read() return bytes , so we have to convert to string and then change it to dict
+    result_dict = loads(json_str)
 
-        format_result = {"names":[] , "expire_date":"" }
+    format_result = {"names": [], "expire_date": ""}
 
-        dates = []
+    dates = []
 
-        result_dict = request.json()
+    for game_info in result_dict:
+        if game_info["discountPrice"] != "0":
+            continue
 
-        for game_info in result_dict:
+        format_result["names"].append(game_info["title"])
+        dates.append(get_date(game_info["offerEndDate"]))
 
-            if game_info["discountPrice"] != "0":
-                break
+    format_result["expire_date"] = get_min_date(dates)
 
-            format_result["names"].append( game_info["title"] )
-            dates.append( get_date(game_info["offerEndDate"]) )
+    return format_result
 
-        format_result[ "expire_date" ] = get_min_date( dates )
-
-
-        return format_result
-
-    else:
-        # return an empty dict to write to json file and prevent error
-        return {}
 
 def get_date(input_string) -> str:
     # yyyy-mm-dd
     return input_string[:10]
 
-def get_min_date(dates) -> str:
 
+def get_min_date(dates) -> str:
     min_date = dates[0]
 
-    def date_compare(date1 , date2):
-        year1 , month1 , day1 = map(int , date1.split("-"))
-        year2 , month2 , day2 = map(int , date2.split("-"))
+    def date_compare(date1, date2):
+        year1, month1, day1 = map(int, date1.split("-"))
+        year2, month2, day2 = map(int, date2.split("-"))
 
         if year1 < year2:
             return date1
@@ -93,18 +98,17 @@ def get_min_date(dates) -> str:
         if month1 < month2:
             return date1
 
-        if month2 < month1 :
+        if month2 < month1:
             return date2
 
         if day1 < day2:
             return date1
 
-        if day2 < day1 :
+        if day2 < day1:
             return date2
 
         # if reaching this point means that 2 date are the same
         return date1
-
 
     for date in dates:
         min_date = date_compare(min_date, date)
--- scripts/get_leetcode_daily_problem.py
+++ scripts/get_leetcode_daily_problem.py
@@ -1,11 +1,13 @@
 from requests import get
-from json import dump , load
+from json import dump, load
 from datetime import date
-from os import chdir
+from os import chdir, path
 
 
 def main():
-    PATH_TO_LEETCODE_DATA = "./data/leetcode_today_problem.json"
+    PATH_TO_LEETCODE_DATA = path.expanduser(
+        "~/.config/eww/data/leetcode_today_problem.json"
+    )
 
     data = None
 
@@ -16,7 +18,10 @@ def main():
 
     try:
         with open(PATH_TO_LEETCODE_DATA, "r") as file_obj:
-            data = load(file_obj)
+            try:
+                data = load(file_obj)
+            except:
+                data = {}
 
         current = date.today()
 
@@ -24,51 +29,50 @@ def main():
 
         # if the json file does not hold anything or the data is outdated
         if "date" not in data or data["date"] != format_date:
-
-
             # update the json
             with open(PATH_TO_LEETCODE_DATA, "w") as file_obj:
                 data = get_leetcode_today_json()
                 dump(data, file_obj)
 
-
     except FileNotFoundError:
-
-
         with open(PATH_TO_LEETCODE_DATA, "w") as file_obj:
-
             data = get_leetcode_today_json()
             dump(data, file_obj)
 
     # yes , I know , this shit happens quite some times
-    format_data = str(data).replace("'" , '"')
+    format_data = str(data).replace("'", '"')
 
-    print( format_data )
+    print(format_data)
+
 
 def get_leetcode_today_json() -> str:
-    request = get('https://alfa-leetcode-api.onrender.com/daily')
+    request = get("https://alfa-leetcode-api.onrender.com/daily")
     if request.status_code == 200:
-
         format_dict = {}
         result_dict = request.json()
 
-        format_dict['link'] = result_dict["questionLink"]
-        format_dict['name'] = result_dict["questionTitle"]
-        format_dict['difficulty'] = result_dict["difficulty"]
-        format_dict['color'] = "green" if format_dict['difficulty'] == "#a3be8c" else "#ebcb8b" if format_dict['difficulty'] == "Medium" else "#bf616a"
-        format_dict['tags'] = []
-        format_dict['date'] = result_dict["date"]
+        format_dict["link"] = result_dict["questionLink"]
+        format_dict["name"] = result_dict["questionTitle"]
+        format_dict["difficulty"] = result_dict["difficulty"]
+        format_dict["color"] = (
+            "#a3be8c"
+            if format_dict["difficulty"] == "Easy"
+            else ("#ebcb8b" if format_dict["difficulty"] == "Medium" else "#bf616a")
+        )
+        format_dict["tags"] = []
+        format_dict["date"] = result_dict["date"]
 
         for tag in result_dict["topicTags"]:
-            format_dict['tags'].append( tag["name"] )
+            format_dict["tags"].append(tag["name"])
 
-        format_dict['tags'] = " , ".join( format_dict["tags"] )
+        format_dict["tags"] = " , ".join(format_dict["tags"])
 
         return format_dict
 
     else:
         # return an empty dict to write to json file and prevent error
         return {}
+
 
 if __name__ == "__main__":
     main()
